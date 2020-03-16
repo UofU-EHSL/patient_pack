d11817ab3744e2d632531c4996fc8d004b7e7b9c
ben <benengel94@gmail.com>
2020-03-16 15:31:40 -0600

    more prefabs and more in the patinet script
    mostly general stuff, nothing is really working yet

--- Patient pack/Assets/Patient Package/Scripts/electrocardiogram.cs
+++ Patient pack/Assets/Patient Package/Scripts/electrocardiogram.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class electrocardiogram : MonoBehaviour
 {
+    private Color color;
+    public LineRenderer line_renderer;
+    public patient_line_renderer custom_line_renderer;
+    public Text[] text_items;
     // Start is called before the first frame update
     void Start()
     {
-        
+        line_renderer.endColor = color;
+        line_renderer.startColor = color;
+        custom_line_renderer.color = color;
+        foreach (Text single in text_items)
+        {
+            single.color = color;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Patient pack/Assets/Patient Package/Scripts/patient.cs
+++ Patient pack/Assets/Patient Package/Scripts/patient.cs
@@ -1,9 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[System.Serializable]
+public class vital
+{
+    public Color color;
+    public LineRenderer line_renderer;  
+    public patient_line_renderer custom_line_renderer;
+    public Text[] text_items;
+}
+
+[ExecuteInEditMode]
 public class patient : MonoBehaviour
 {
+    public vital[] Vitals_visuals;
     public electrocardiogram ecg;
     public oxygen_saturation o2;
     public capnography Co2;
@@ -11,4 +23,28 @@ public class patient : MonoBehaviour
     public blood_pressure bp;
     public temperature temp;
     public blood_volume bloodVolume;
+
+    public void updateViz()
+    {
+        foreach (vital single in Vitals_visuals)
+        {
+            single.line_renderer.endColor = single.color;
+            single.line_renderer.startColor = single.color;
+            single.custom_line_renderer.color = single.color;
+            foreach (Text one in single.text_items)
+            {
+                one.color = single.color;
+            }
+        }
+    }
+    public void Start()
+    {
+        updateViz();
+    }
+    public void Update()
+    {
+    #if UNITY_EDITOR
+        updateViz();
+    #endif
+    }
 }
